49aa8335837bcf714bb8a6d0f93a391614f0980d
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-05 12:57:23 +0200

    changed sanity naming projectId and dataset

--- dist/static/index-1dce9e00-c5cbad46.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-504fa488-4b569863.js";import{D as m}from"./sanity-12b52a7a.js";import"./json-inspector-e75399e6.js";export{m as DocumentPaneProvider,t as default};
