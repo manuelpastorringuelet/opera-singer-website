27601c350426985af06e72767530735bde3eb792
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-09 00:14:01 +0200

    added cursor pointer to pages

--- components/menubar.tsx
+++ components/menubar.tsx
@@ -30,14 +30,14 @@ export function MenuBar() {
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger className="bg-transparent hover:bg-primary/90 hover:text-primary-foreground z-1-">
+          <NavigationMenuTrigger className="z-1- bg-transparent hover:bg-primary/90 hover:text-primary-foreground">
             <Menu className="group-data-[state=open]:hidden" />
             <X className="hidden group-data-[state=open]:block" />
           </NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-3 px-6 py-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <NavigationMenuLink
-                className="py-2"
+                className="cursor-pointer py-2"
                 onClick={() => router.push("/")}
               >
                 HOME
@@ -45,7 +45,7 @@ export function MenuBar() {
               {pages.map((page) =>
                 page.href ? (
                   <NavigationMenuLink
-                    className="py-2"
+                    className="cursor-pointer py-2"
                     key={page.name}
                     onClick={() => router.push(page.href as string)}
                   >
@@ -63,7 +63,7 @@ export function MenuBar() {
                       <AccordionContent>
                         {page.children?.map((child) => (
                           <NavigationMenuLink
-                            className="flex flex-col justify-center py-2 pl-2"
+                            className="flex cursor-pointer flex-col justify-center py-2 pl-2"
                             key={child.name}
                             onClick={() => router.push(child.href as string)}
                           >
