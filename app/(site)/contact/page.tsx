dbe7212b2610f1a53dca96fa8c873567b6dd6d34
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-11 10:53:28 +0200

    set up calendar, music and contact pages

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,8 +1,71 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z.string().nonempty(),
+  email: z.string().email(),
+  subject: z.string().optional(),
+  message: z.string().nonempty(),
+});
+
 const Contact = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
-    <div className="max-w-7xl mx-auto lg:px-16 px-6 flex items-center justify-center flex-1">
-      <h1>Contact Page</h1>
-    </div>
+    <section className="container flex flex-1 flex-col items-center gap-8 py-8">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </section>
   );
 };
 
--- app/(site)/music/page.tsx
+++ app/(site)/music/page.tsx
@@ -1,8 +1,9 @@
 const Music = () => {
   return (
-    <div className="max-w-7xl mx-auto lg:px-16 px-6 flex items-center justify-center flex-1">
-      <h1>Music Page</h1>
-    </div>
+    <section className="container flex flex-1 flex-col items-center gap-8 py-8">
+      <audio src="/listen.m4a" controls />
+      <video src="watch.m4v" controls />
+    </section>
   );
 };
 
