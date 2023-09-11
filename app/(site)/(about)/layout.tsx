8a31c666db03229e77f5abc331e912a8d4199a9e
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-11 14:17:44 +0200

    added colum on lg size

--- app/(site)/(about)/layout.tsx
+++ app/(site)/(about)/layout.tsx
@@ -6,9 +6,11 @@ const AboutLayout = ({
     children: React.ReactNode;
   }) => {
     return ( 
-        <section className="container flex flex-1 flex-col gap-8">
+        <section className="container flex flex-1 flex-col gap-8 px-16">
         <AboutImage />
+      <p className="pb-4 text-justify text-sm font-thin gap-8 md:columns-2 lg:columns-3 md:text-base">
        {children}
+      </p>
       </section>
      );
 }
--- components/about-image.tsx
+++ components/about-image.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const AboutImage = () => {
   return (
-    <div className="h-40 overflow-clip object-cover object-top md:h-80">
+    <div className="h-40 overflow-clip object-cover object-top md:h-80 w-full">
       <Image
         src="/hero.png"
         width={1000}
