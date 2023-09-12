a3072c8e26b3e1aa2ce74abeacf64aa87f37fc91
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-12 22:53:38 +0200

    dummy impressum & datenschutzerklarung

--- app/(site)/impressum/page.tsx
+++ app/(site)/impressum/page.tsx
@@ -1,8 +1,94 @@
 const Impressum = () => {
   return (
-    <div className="max-w-7xl mx-auto lg:px-16 px-6 flex items-center justify-center flex-1">
-      <h1>Impressum Page</h1>
-    </div>
+    <section className="container mx-auto flex max-w-7xl flex-1 flex-col  gap-4 px-16 py-8 text-start text-sm">
+      <h1 className="text-2xl font-semibold">Impressum</h1>
+
+      <section>
+        <p>Angaben gemäß § 5 TMG:</p>
+        <p>Taryn Knerr</p>
+        <p>1234 Musterstadt</p>
+        <p>E-Mail: taryn.knerr@example.com</p>
+      </section>
+
+      <h2 className="text-xl font-semibold">Haftungsausschluss</h2>
+
+      <section className="flex flex-col gap-1">
+        <h3 className="underline">Haftung für Inhalte</h3>
+        <p>
+          Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für
+          die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir
+          jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7
+          Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen
+          Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als
+          Diensteanbieter jedoch nicht verpflichtet, übermittelte oder
+          gespeicherte fremde Informationen zu überwachen oder nach Umständen zu
+          forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+          Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+          Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
+          Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+          Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden
+          von entsprechenden Rechtsverletzungen werden wir diese Inhalte
+          umgehend entfernen.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-1">
+        <h3 className="underline">Haftung für Links</h3>
+        <p>
+          Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren
+          Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
+          fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+          verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
+          Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+          Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige
+          Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine
+          permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne
+          konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+          Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+          umgehend entfernen.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-1">
+        <h3 className="underline">Urheberrecht</h3>
+        <p>
+          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+          Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
+          Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+          Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+          jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
+          sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+          Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt
+          wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden
+          Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf
+          eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+          entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+          werden wir derartige Inhalte umgehend entfernen.
+        </p>
+      </section>
+
+      <section className="flex flex-col gap-1">
+        <h3 className="underline">Datenschutz</h3>
+        <p>
+          Die Nutzung unserer Webseite ist in der Regel ohne Angabe
+          personenbezogener Daten möglich. Soweit auf unseren Seiten
+          personenbezogene Daten (beispielsweise Name, Anschrift oder
+          E-Mail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets
+          auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche
+          Zustimmung nicht an Dritte weitergegeben. Wir weisen darauf hin, dass
+          die Datenübertragung im Internet (z.B. bei der Kommunikation per
+          E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der
+          Daten vor dem Zugriff durch Dritte ist nicht möglich. Der Nutzung von
+          im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch
+          Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung
+          und Informationsmaterialien wird hiermit ausdrücklich widersprochen.
+          Die Betreiber der Seiten behalten sich ausdrücklich rechtliche
+          Schritte im Falle der unverlangten Zusendung von Werbeinformationen,
+          etwa durch Spam-Mails, vor.
+        </p>
+      </section>
+      <p>Quellenangaben: Disclaimer eRecht24</p>
+    </section>
   );
 };
 
