759c38a713869fc2bc7b7be7d6a35259d13d136d
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-22 22:04:31 +0200

    removed horizontal bar

--- app/(site)/calendar/page.tsx
+++ app/(site)/calendar/page.tsx
@@ -2,6 +2,9 @@ import { getPerformances } from "@/sanity/sanity.query";
 
 import Performance from "@/components/performance";
 
+// Helper function to convert dates to JavaScript Date objects
+const convertToDates = (dates: Date[]) => dates.map((date) => new Date(date));
+
 const Calendar = async () => {
   const performances = await getPerformances();
 
@@ -17,9 +20,7 @@ const Calendar = async () => {
   const years = [
     ...new Set(
       performances.map((performance) =>
-        new Date(
-          performance.dates.map((date) => new Date(date))[0],
-        ).getFullYear(),
+        convertToDates(performance.dates)[0].getFullYear(),
       ),
     ),
   ];
@@ -27,36 +28,28 @@ const Calendar = async () => {
   return (
     <main className="container flex flex-1 flex-col items-center gap-8 py-8  text-center">
       {years.map((year, index) => (
-        <>
-          <section
-            key={index}
-            className="container flex flex-col items-start justify-center gap-4 font-light sm:flex sm:flex-row md:gap-10 lg:gap-20"
-          >
-            <h1 className="text-4xl font-semibold text-primary/80 sm:text-5xl">
-              {year}
-            </h1>
-            <div className="flex flex-col gap-8">
-              {performances
-                .filter(
-                  (performance) =>
-                    performance.dates
-                      .map((date) => new Date(date))[0]
-                      .getFullYear() === year,
-                )
-                .map((performance, index) => (
-                  <>
-                    <Performance key={index} {...performance} />
-                  </>
-                ))}
-            </div>
-          </section>
-          {
-            // add a horizontal rule between each year, except the last
-            index !== years.length - 1 && (
-              <hr className="w-full opacity-50 sm:w-2/3" />
-            )
-          }
-        </>
+        <section
+          key={index}
+          className="container flex flex-col items-start justify-center gap-4 font-light sm:flex sm:flex-row md:gap-10 lg:gap-20"
+        >
+          <h1 className="text-4xl font-semibold text-primary/80 sm:text-5xl">
+            {year}
+          </h1>
+          <div className="flex flex-col gap-8">
+            {performances
+              .filter(
+                (performance) =>
+                  performance.dates
+                    .map((date) => new Date(date))[0]
+                    .getFullYear() === year,
+              )
+              .map((performance, index) => (
+                <>
+                  <Performance key={index} {...performance} />
+                </>
+              ))}
+          </div>
+        </section>
       ))}
     </main>
   );
