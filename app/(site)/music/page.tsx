cf5ff9e59240acf29359a0c8969a76e487128f9e
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-22 19:32:27 +0200

    fixed music layout

--- app/(site)/music/page.tsx
+++ app/(site)/music/page.tsx
@@ -12,21 +12,11 @@ const Music = async () => {
 
   return (
     <section className="container mx-auto flex flex-1 flex-col gap-6 py-8 sm:px-16 ">
-      <section className="grid grid-cols-music gap-2">
+      <section className="grid gap-4 sm:grid-cols-music">
         <MusicPlayer songs={songs} />
-        {/* <div className="relative overflow-hidden rounded-xl">
-          <Image
-            priority
-            src={profile.heroImage.image}
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 70vw"
-            alt={profile.heroImage.alt}
-            className=" object-cover"
-          />
-        </div> */}
         {videos.map((video, index) => (
-          <div key={index} className="relative overflow-hidden rounded-xl">
-            <video src={video.file} muted controls className="h-full" />
+          <div key={index} className="overflow-hidden rounded-xl">
+            <video src={video.file} muted controls />
           </div>
         ))}
       </section>
