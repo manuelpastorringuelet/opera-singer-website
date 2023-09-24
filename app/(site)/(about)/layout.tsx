e203c7fa3958ff8401519c23f69ffadbd2128fad
Manuel Pastor <pastorringuelet@gmail.com>
2023-09-24 12:56:21 +0200

    changed about from grid to flex

--- app/(site)/(about)/layout.tsx
+++ app/(site)/(about)/layout.tsx
@@ -13,7 +13,7 @@ const AboutLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="container flex flex-1 flex-col gap-3 sm:px-16 md:gap-8">
       <AboutImage about={about} />
-      <section className="grid gap-16 pb-4 text-sm md:grid-cols-critics md:text-base">
+      <section className="flex flex-col gap-16 pb-4 text-sm md:grid md:grid-cols-critics md:text-base">
         <Critics critics={critics} />
         {children}
       </section>
