7dfb79d6cc87370a732ba891375c93b0217fac81
Manuel Pastor <pastorringuelet@gmail.com>
2023-10-19 10:00:23 +0200

    added suppressHydrationWarning to calendar page

--- app/(site)/calendar/page.tsx
+++ app/(site)/calendar/page.tsx
@@ -6,7 +6,10 @@ const Calendar = async () => {
   const performances = await getPerformances();
 
   return (
-    <main className="flex flex-1 flex-col items-center gap-8 py-8 text-center  sm:container">
+    <main
+      suppressHydrationWarning
+      className="flex flex-1 flex-col items-center gap-8 py-8 text-center  sm:container"
+    >
       <Performances performances={performances} />
     </main>
   );
