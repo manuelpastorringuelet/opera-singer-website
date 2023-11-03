a725c7e6a5f2a64f33bd9e9c5bd1bc3c8b376319
Manuel Pastor <pastorringuelet@gmail.com>
2023-11-03 20:37:25 +0100

    ft:(hero) 2 columns on md to better display text

--- components/hero.tsx
+++ components/hero.tsx
@@ -16,7 +16,7 @@ export default function Hero({ profile }: Props) {
   const { theme } = useTheme();
 
   return (
-    <main className="mx-auto grid w-full flex-1 justify-items-end items-center overflow-y-auto overflow-x-hidden sm:container sm:grid-cols-2">
+    <main className="mx-auto grid w-full flex-1 items-center justify-items-end overflow-y-auto overflow-x-hidden md:container md:grid-cols-2">
       <motion.section
         initial={{
           x: 300,
@@ -29,7 +29,7 @@ export default function Hero({ profile }: Props) {
         transition={{
           duration: 1.2,
         }}
-        className="z-10 sm:mx-0 mx-auto flex flex-col items-start gap-3 self-center overflow-hidden px-8 sm:justify-self-center sm:px-0 md:ml-auto"
+        className="z-10 mx-auto flex flex-col items-start gap-3 self-center overflow-hidden px-8 sm:mx-0 sm:justify-self-center sm:px-0 md:ml-auto"
       >
         <h1
           className={cn(
